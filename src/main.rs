8fe6da3b1f2e47f87b92e5b2c7263494650b00f8
Vinit Hemadri <vineet.vh@gmail.com>
2022-01-17 14:26:26 +0530

    Added nonogram program

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-mod sum_of_values_of_letters;
+// mod sum_of_values_of_letters;
+mod nonogram_row;
 
 fn main() {
-   sum_of_values_of_letters::sum_of_values();
+   // sum_of_values_of_letters::sum_of_values();
+   nonogram_row::nonogram_row();
 }
---
+++ src/nonogram_row.rs
@@ -0,0 +1,7 @@
+pub fn nonogram_row() {
+  let arr: [u32; 5] = [1,1,1,1,1];
+
+  for val in arr {
+    println!("{}", val);
+  }
+}
